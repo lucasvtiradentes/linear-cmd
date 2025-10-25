8168a54c31cd222dfe92006a5e0767bbd965a9a0
Lucas Vieira <lucasvtiradentes@gmail.com>
2025-10-25 00:19:05 -0300

    refactor: rename project command files

--- src/commands/project/index.ts
+++ src/commands/project/index.ts
@@ -2,11 +2,11 @@ import { Command } from 'commander';
 import { CommandNames } from '../../schemas/definitions.js';
 import { createCommandFromSchema } from '../../schemas/utils.js';
 
-import { createCreateProjectCommand } from './create-project.js';
-import { createDeleteProjectCommand } from './delete-project.js';
-import { createListProjectIssuesCommand } from './list-project-issues.js';
-import { createListProjectsCommand } from './list-projects.js';
-import { createShowProjectCommand } from './show-project.js';
+import { createCreateProjectCommand } from './create.js';
+import { createDeleteProjectCommand } from './delete.js';
+import { createListProjectIssuesCommand } from './issues.js';
+import { createListProjectsCommand } from './list.js';
+import { createShowProjectCommand } from './show.js';
 
 export function createProjectCommand(): Command {
   const project = createCommandFromSchema(CommandNames.PROJECT);
